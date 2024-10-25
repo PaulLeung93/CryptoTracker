225074a9dd9cda3e081ed8887b53fd8fd684415e
Paul Leung <78509075+PaulLeung93@users.noreply.github.com>
2024-10-25 16:31:26 -0400

    Added model classes for our Coin UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.plcoding.cryptotracker"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.plcoding.cryptotracker"
