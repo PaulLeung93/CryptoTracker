4e509ff184ed1f1317fb815fe1e5c46dd81bf0c9
Paul Leung <78509075+PaulLeung93@users.noreply.github.com>
2024-10-27 17:56:03 -0400

    Implemented UI for individual Coin Items, representing the Icon, name, Price, and Price change of each Coin item

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinUi.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinUi.kt
@@ -1,6 +1,11 @@
 package com.plcoding.cryptotracker.crypto.presentation.models
 
+import android.icu.number.NumberFormatter
 import androidx.annotation.DrawableRes
+import com.plcoding.cryptotracker.crypto.domain.Coin
+import com.plcoding.cryptotracker.util.getDrawableIdForCoin
+import java.text.NumberFormat
+import java.util.Locale
 
 data class CoinUi(
     val id: String,
@@ -17,3 +22,27 @@ data class DisplayableNumber(
     val value: Double,
     val formatted: String
 )
+
+fun Coin.toCoinUi(): CoinUi {
+    return CoinUi(
+        id = id,
+        rank = rank,
+        name = name,
+        symbol = symbol,
+        marketCapUsd = marketCapUsd.toDisplayableNumber(),
+        priceUsd = priceUsd.toDisplayableNumber(),
+        changePercent24Hr = changePercent24Hr.toDisplayableNumber(),
+        iconRes = getDrawableIdForCoin(symbol)
+    )
+}
+
+fun Double.toDisplayableNumber(): DisplayableNumber {
+    val formatter = NumberFormat.getNumberInstance(Locale.getDefault()).apply {
+        minimumFractionDigits = 2
+        maximumFractionDigits = 2
+    }
+    return DisplayableNumber(
+        value = this,
+        formatted = formatter.format(this)
+    )
+}
